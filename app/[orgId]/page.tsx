6a3f2848b23619f738016643b5aef607818fd4e1
SpyC0der77 <carter.stach@gmail.com>
2025-09-06 16:17:40 -0400

    Remove orgs

--- app/[orgId]/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default async function OrgIdPage({ params }: { params: Promise<{ orgId: string }> }) {
-   const { orgId } = await params;
-   redirect(`${orgId}/inbox`);
-}
